8fc74fed646d2066e438c54506161a3ec507cde3
Neeleshnishant <103322503+Neeleshnishant@users.noreply.github.com>
2022-05-17 14:32:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const btnNavEl = document.querySelector(".btn-menu-close");
+const headerEl = document.querySelector(".header-logo");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("open-nav");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("open-nav");
+  });
+});
